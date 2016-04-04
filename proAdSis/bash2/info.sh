e79995d7a1eeacb4c6452444d31e25889482d009
toniPortillo <ceox1992@gmail.com>
2016-04-04 17:32:20 +0200

    script para ver informacion de equipo y usuario

---
+++ proAdSis/bash2/info.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -e "Bienvenido $USER!, tu identificador es $UID.
+Esta es la shell número $SHLVL, que lleva $SECONDS arrancada.
+La arquitectura de esta máquina es $MACHTYPE y el cliente de
+terminal es $TERM"
