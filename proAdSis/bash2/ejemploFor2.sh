2f7b418814f978ace1fcb8f4f469a674c2c5010c
toniPortillo <ceox1992@gmail.com>
2016-04-11 11:12:01 +0200

    Scripts con mas ejemplos de la estructura iterativa for

---
+++ proAdSis/bash2/ejemploFor2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for x in papel lapiz boligrafo
+do
+  echo "El valor de la variable es $x"
+  sleep 2
+done
---
+++ proAdSis/bash2/ejemploFor2Bis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for x in "papel dorado" "lapiz caro" "boligrafo barato"
+do
+  echo "El valor de la variable es $x"
+  sleep 2
+done
