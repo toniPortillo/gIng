f16a84b21d2a4219304041139be4ca316ccf1e72
toniPortillo <ceox1992@gmail.com>
2016-04-12 11:17:26 +0200

    Script con ejemplo de funcion

---
+++ proAdSis/bash2/funcionHola.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hola()
+{
+  echo "Estas dentro de la función hola()"
+}
+
+echo "En la siguiente linea se ejecutara la función hola()"
+hola
+echo "Estas fuera de la función hola()"
