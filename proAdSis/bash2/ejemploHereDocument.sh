e416be4164da9f0d02b49f723437cbaf416334f2
toniPortillo <ceox1992@gmail.com>
2016-04-13 17:20:49 +0200

    Script ejemplo de here documents

---
+++ proAdSis/bash2/ejemploHereDocument.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Sin here documents
+echo "****************"
+echo "* Mi script V1 *"
+echo "****************"
+echo "Introduzca su nombre"
+echo "                      "
+
+#Usando here documents
+cat << EOF
+****************
+* Mi script V1 *
+****************
+Introduzca su nombre
+EOF
