7a7e1b3aa028f792cb11ad27ebe9dcfa77f60245
toniPortillo <ceox1992@gmail.com>
2016-04-16 19:30:24 +0200

    Script ejemplo del comando find

---
+++ proAdSis/bash2/ejemploFind.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+find /home/toni/Documentos/Universidad2016/gIng/proAdSis/bash2 -name "*.sh"
+echo -e "\n"
+
+find /home/toni/Documentos/Universidad2016/gIng/proAdSis/bash2 -size 1024c
+echo -e "\n"
+
+find /home/toni/Documentos/Universidad2016/gIng/proAdSis/bash2 -user toni
+echo -e "\n"
