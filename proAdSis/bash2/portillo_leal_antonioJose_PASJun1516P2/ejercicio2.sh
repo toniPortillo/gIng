6b64b77ec5f7a5208e511115ef8cb1532e9e7ee4
toniPortillo <ceox1992@gmail.com>
2016-04-25 15:52:16 +0200

    Ultimo script bash bas6.sh y carpeta de entrega con todos

---
+++ proAdSis/bash2/portillo_leal_antonioJose_PASJun1516P2/ejercicio2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ $# -le "0" ] || [ $# -gt "3" ]
+then
+  echo "El numero de parametros tiene que ser de 1 a 3"
+  echo "Formato ./ejercicio2.sh directorio umbral1 umbral2(opcionales)"
+else
+
+  if [ -d "pequenos" ] && [ -d "medianos" ] && [ -d "grandes" ]
+  then
+    echo "Las carpetas de salida ya existen, se va a proceder a borrarlas..."
+    echo "Copiando los archivos..."
+    rm -R pequenos medianos grandes
+    mkdir pequenos medianos grandes
+  else
+    echo "Creando las carpetas pequenos, medianos y grande..."
+    echo "Copiando los archivos..."
+    mkdir pequenos medianos grandes
+  fi
+
+  if [ $# -eq "1" ]
+  then
+    umbral1=10000
+    umbral2=100000
+  else
+    umbral1=$2
+    umbral2=$3
+  fi
+
+  find $1 -type f | while read f
+  do
+    if [ -f $f ]
+    then
+      if [ $(stat -c%s $f) -lt $umbral1 ]
+      then
+        cp $f pequenos
+      elif [ $(stat -c%s $f) -ge $umbral1 ] && [ $(stat -c%s $f) -le $umbral2 ]
+      then
+        cp $f medianos
+      else
+        cp $f grandes
+      fi
+    fi
+  done
+fi
